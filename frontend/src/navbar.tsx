fa03bbebd52f107961f7b17bed9add4110c68c65
RusselllN <90791414+RusselllN@users.noreply.github.com>
2026-01-27 15:03:09 -0500

    made a nav bar

---
+++ frontend/src/navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+
+return(
+    <div>
+        <ul className="flex px-15 py-8 gap-4 bg-gray-200 w-screen justify-end">
+            <li className="bg-blue-400 px-2 py-1 rounded-2xl"><a>CIPR Instructions</a></li>
+            <li className="bg-blue-400 px-2 py-1 rounded-2xl"><a>Videos</a></li>
+            <li className="bg-blue-400 px-2 py-1 rounded-2xl"><a>Add Member</a></li>
+        </ul>
+    </div>
+)
+
+}
