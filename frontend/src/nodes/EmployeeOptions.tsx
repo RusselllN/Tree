ac4424c9515da9e75cbb7e7fb11a06de39088c17
RusselllN <90791414+RusselllN@users.noreply.github.com>
2026-01-28 13:19:04 -0500

    custom node css

--- frontend/src/nodes/EmployeeOptions.tsx
+++ frontend/src/nodes/EmployeeOptions.tsx
@@ -3,7 +3,7 @@ import { Position, Handle } from "@xyflow/react";
 export function EmployeeOptions() {
   return (
     <div className="button-person">
-      <div className="hover">
+      <div className="border rounded-lg px-15 py-2">
         <button onClick={() => alert(1)}>Jerry</button>
       </div>
       <button onClick={() => alert(2)}>Add Child</button>
