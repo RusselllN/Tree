93978ffdcf0d042ef56be7c174fe27564b95cad1
RusselllN <90791414+RusselllN@users.noreply.github.com>
2026-01-28 17:20:51 -0500

    changed something

--- frontend/src/nodes/EmployeeOptions.tsx
+++ frontend/src/nodes/EmployeeOptions.tsx
@@ -4,7 +4,7 @@ export function EmployeeOptions({id, data}) {
   return (
     <div className="button-person">
       <div className="border rounded-lg px-10 py-2">
-        <button onClick={() => alert(1)}>Jerry</button>
+        <button onClick={() => alert(1)}>{data.label}</button>
       </div>
       <button onClick={() => alert(2)}>Add Child</button>
       <Handle type="target" position={Position.Top} />
