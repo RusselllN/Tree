ded48bb30190bcbd6a9153d7f5a0cf5dc3f4038c
RusselllN <90791414+RusselllN@users.noreply.github.com>
2026-01-28 17:21:17 -0500

    added function for new nodes and some refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,50 @@
 
 import { useState, useCallback } from 'react';
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Background, Panel} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { TextUpdaterNode } from './nodes/TextUpdaterNode';
+
 import { EmployeeOptions } from './nodes/EmployeeOptions';
 import { Position, Handle } from '@xyflow/react'; // for connecting nodes
- 
-const initialNodes = [
-  { id: 'n1', type: 'employee', position: { x: 0, y: 0 }, data: { label: 'Jerry' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Jack' } },
-  { id: 'n3', position: { x: 250, y: 100}, data: {label: 'Jill'}},
-  { id: 'n4', position: { x: -250, y: 100}, data: {label: 'Supercalifragalisticexpialidocious'}}
-];
-const initialEdges = [
-  { id: 'n1-n2', source: 'n1', target: 'n2' },
-  { id: 'n1-n3', source: 'n1', target: 'n3' },
-  { id: 'n1-n4', source: 'n1', target: 'n4' }
-];
 
+import { shallow } from 'zustand/shallow';
+import { useStore } from './store.js';
+
+// store node data
+const selector = (store) => ({
+  nodes: store.nodes,
+  edges: store.edges,
+  onNodesChange: store.onNodesChange,
+  onEdgesChange: store.onEdgesChange,
+  addEdge: store.addEdge,
+  onNodesDelete: store.removeNodes, // for deleting nodes
+  createNode: store.createNode
+});
+
+// custom node types
+const nodeTypes = {
+  employee: EmployeeOptions
+};
 
 // Build Nodes
 export default function App() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
-  
-  // custom node types
-  const nodeTypes = {
-    textUpdater: TextUpdaterNode,
-    employee: EmployeeOptions,
-  };
- 
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
-    [],
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
-    [],
-  );
-  const onConnect = useCallback(
-    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
-    [],
-  );
- 
+  const store = useStore(selector, shallow);
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      
       <ReactFlow
-        nodes={nodes}
-        edges={edges}
+        nodes={store.nodes}
+        edges={store.edges}
         nodeTypes={nodeTypes} // unique node type
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      />
+        onNodesChange={store.onNodesChange}
+        onEdgesChange={store.onEdgesChange}
+        onConnect={store.addEdge}
+        onNodesDelete={store.onNodesDelete} // for deleting nodes
+      >
+        <Panel position="top-right">
+          <button className="px-5" onClick={() => store.createNode('employee')}>New Employee</button>
+        </Panel>
+        <Background />
+      </ReactFlow>
     </div>
   );
 }
